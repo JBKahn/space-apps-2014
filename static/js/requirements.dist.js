8b47dba4ba463a364fba1b56ba22ce2c96afe58c
Joseph <jkahn@waveaccounting.com>
2014-04-13 10:01:11 -0400

    fixed some styles

--- static/js/requirements.dist.js
+++ static/js/requirements.dist.js
@@ -9155,8 +9155,8 @@ Vitals = (function () {
         if (this.startAngle1 >= 1.56) {
             this.startAngle1 = null;
         }
-        this.startAngle1 = this.drawVital($("#vitals").height() * 0.85, $("#vitals").height() * 0.05, 1, this.startAngle1, 0.005, "#00ff66", true, '/static/images/o2_360.png', $("#vitals").height() * 0.13);
-        this.startAngle2 = this.drawVital($("#vitals").height() * 0.75, $("#vitals").height() * 0.05, 1, this.startAngle2, 0.25, "#ffea00", false, '/static/images/energy_360.png', $("#vitals").height() * 0.23);
+        this.startAngle1 = this.drawVital($("#vitals").height() * 0.85, $("#vitals").height() * 0.05, 1, this.startAngle1, 0.005, "#00ff66", true, '/static/images/o2_360.png', $("#vitals").height() * 0.07);
+        this.startAngle2 = this.drawVital($("#vitals").height() * 0.70, $("#vitals").height() * 0.05, 1, this.startAngle2, 0.25, "#ffea00", false, '/static/images/energy_360.png', $("#vitals").height() * 0.23);
     };
 
     Vitals.prototype.drawVital = function (radius, width, direction, midValue, variance, color, strictlyDecrease, image_path, image_height_offset) {
@@ -9181,7 +9181,7 @@ Vitals = (function () {
         if (image_path) {
             imageObj = new Image();
             imageObj.src = image_path;
-            this.context.drawImage(imageObj, this.centerX - 50, image_height_offset);
+            this.context.drawImage(imageObj, this.centerX - 25, image_height_offset);
         }
 
         if (invert) {
@@ -9196,11 +9196,11 @@ Vitals = (function () {
         var size = $("#vitals").height() * 0.12;
         this.context.font = "bold " + String(size) + "px Georgia white";
         this.context.fillStyle = "#FFF";
-        this.context.fillText(String(hour) + ":" + String(minutes), 60, $("#vitals").height() * 0.66);
+        this.context.fillText(String(hour) + ":" + String(minutes), 60, $("#vitals").height() * 0.73);
 
         imageObj = new Image();
         imageObj.src = "/static/images/clock_360.png";
-        this.context.drawImage(imageObj, this.centerX - 50, $("#vitals").height() * 0.63);
+        this.context.drawImage(imageObj, this.centerX - 25, $("#vitals").height() * 0.63);
 
         this.context.beginPath();
         this.context.shadowColor   = color;
@@ -9210,7 +9210,7 @@ Vitals = (function () {
 
         this.context.lineWidth = width;
         this.context.strokeStyle = color;
-        this.context.translate(this.centerX, this.centerY);
+        this.context.translate(this.centerX + 25, this.centerY);
         this.context.rotate(-90 * (Math.PI / 180));
         this.context.arc(x, y, radius, startAngle, endAngle, direction);
         this.context.stroke();
@@ -9228,7 +9228,7 @@ $(document).ready(function () {
     $vitals = $("#vitals");
 
     $vitals[0].height = window.innerHeight * 0.25;
-    $vitals[0].width = window.innerHeight * 0.25;
+    $vitals[0].width = window.innerHeight * 0.35;
 
     vitals = new Vitals($vitals);
 
@@ -9240,25 +9240,23 @@ $(document).ready(function () {
     document.getElementById('infoBoxTopRight').width = window.innerHeight * 0.25;
     var topRightInfoSideSize = window.innerHeight * 0.25;
 
-    document.getElementById('infoBoxBottomRight').height = window.innerHeight * 0.15;
+    document.getElementById('infoBoxBottomRight').height = window.innerHeight * 0.25;
     document.getElementById('infoBoxBottomRight').width = window.innerHeight * 0.40;
-    var bottomRightInfoHeightSize = window.innerHeight * 0.15;
+    var bottomRightInfoHeightSize = window.innerHeight * 0.25;
     var bottomRightInfoWidthSize = window.innerHeight * 0.40;
 
     $('.zoomed-in-object img').width(bottomRightInfoWidthSize);
-    $('.doge-far-away-text').css('bottom', window.innerHeight * 0.04);
-    $('.doge-far-away-text').css('left', window.innerHeight * 0.05);
+    $('.zoomed-in-object img').css('position', 'absolute');
+    $('.zoomed-in-object img').css('bottom', String(window.innerHeight * 0.25 + 10) + 'px');
+
+    $('.doge-far-away-text').css('bottom', window.innerHeight * 0.06);
+    $('.doge-far-away-text').css('left', window.innerHeight * 0.1);
     $('.doge-far-away-text').css('font-size', window.innerHeight * 0.02);
     $('.mini-map').css("margin-top", -topRightInfoSideSize);
     $('.mini-map').css("height", topRightInfoSideSize * 0.75);
 
-
-
-
-
     var topRightPaths = [[30, 0], [topRightInfoSideSize, 0], [topRightInfoSideSize, topRightInfoSideSize * 0.9], [topRightInfoSideSize * 0.4, topRightInfoSideSize * 0.9], [30, topRightInfoSideSize * 0.6]],
         bottomRightPaths = [[30, 30], [bottomRightInfoWidthSize, 30], [bottomRightInfoWidthSize, bottomRightInfoHeightSize * 0.9], [bottomRightInfoWidthSize * 0.2, bottomRightInfoHeightSize * 0.9], [30, bottomRightInfoHeightSize * 0.8]];
-
 
     var drawSidePoly = function (paths, boxId) {
         var c2 = document.getElementById(boxId).getContext('2d');
